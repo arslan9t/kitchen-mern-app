3284a0248b0752078a49e36e4d064d50362e4f86
arslan ali <arslanali9t@gmail.com>
2024-08-25 01:23:21 +0530

    23

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -48,7 +48,9 @@ const StoreContextProvider = (props) => {
 		for (const item in cartItems) {
 			if (cartItems[item]) {
 				let d = food_list.find((food) => food._id === item);
-				total += cartItems[item] * d.price;
+				if (d) {
+					total += cartItems[item] * d.price;
+				}
 			}
 		}
 		return total;
