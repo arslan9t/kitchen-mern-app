a86c00c849fe11195dc5be67dfb6130a3b6f9746
arslan ali <arslanali9t@gmail.com>
2024-08-24 11:49:43 +0530

    17

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -167,9 +167,7 @@ const order = () => {
 						onClick={() => navigate("/myorder")}
 						className="p-[1vw] mt-[1vw] bg-orange-700 rounded text-white"
 					>
-						<p className="text-[1vw]" onClick={() => setCartItems({})}>
-							Place Order
-						</p>
+						<p className="text-[1vw]">Place Order</p>
 					</button>
 				</div>
 			</form>
